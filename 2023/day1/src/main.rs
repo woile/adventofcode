1973fa60acc3aafce2ab1212bc38d96816591af7
Santiago Fraire <santiwilly@gmail.com>
2023-12-01 13:15:58 +0000

    day 1: challenge 2

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,32 +1,55 @@
 use std::fs;
 
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take, take_while1},
+    combinator::{peek, value},
+    multi::many0,
+    IResult,
+};
+
+fn parse_str_num(i: &str) -> IResult<&str, &str> {
+    let (tail, out) = peek(alt((
+        value("1", tag("one")),
+        value("2", tag("two")),
+        value("3", tag("three")),
+        value("4", tag("four")),
+        value("5", tag("five")),
+        value("6", tag("six")),
+        value("7", tag("seven")),
+        value("8", tag("eight")),
+        value("9", tag("nine")),
+    )))(i)?;
+    let (tail, _) = take(1usize)(tail)?;
+    Ok((tail, out))
+}
+fn parse_num(i: &str) -> IResult<&str, Vec<&str>> {
+    many0(alt((
+        take_while1(move |c: char| c.is_numeric()),
+        parse_str_num,
+        value("", take(1usize)),
+    )))(i)
+}
+
+fn parser(i: &str) -> i32 {
+    println!("{}", i);
+    let (_, num) = parse_num(i).expect("numer not returned");
+    let binding = num.join("");
+    let mut chars = binding.trim().chars();
+    println!("{:?}", chars);
+    let first = chars.next().unwrap_or('0');
+    let last = chars.next_back().unwrap_or(first);
+    let val = format!("{}{}", first, last).parse::<i32>().unwrap();
+    println!("{}", val);
+    println!("-----------------");
+    val
+}
+
 fn main() {
     let result: i32 = fs::read_to_string("input.txt")
         .expect("Something went wrong reading the file")
         .lines()
-        .map(|line| {
-            let mut first: Option<char> = None;
-            let mut last: Option<char> = None;
-            for l in line.chars() {
-                if l.is_digit(10) {
-                    if first.is_none() {
-                        first = Some(l);
-                    } else {
-                        last = Some(l);
-                    }
-                }
-            }
-            let r = match (first, last) {
-                (Some(firstv), Some(lastv)) => {
-                    format!("{}{}", firstv, lastv).parse::<i32>().unwrap()
-                }
-                (Some(firstv), None) => format!("{}{}", firstv, firstv).parse::<i32>().unwrap(),
-                (None, Some(lastv)) => format!("{}{}", lastv, lastv).parse::<i32>().unwrap(),
-                _ => 0,
-            };
-            println!("{}", r);
-            r
-        })
+        .map(|line| parser(line))
         .collect::<Vec<i32>>()
         .iter()
         .sum();
