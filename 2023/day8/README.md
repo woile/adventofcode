615c07693206c8c5287c77da462db90da574eddf
Santiago Fraire <santiwilly@gmail.com>
2023-12-12 08:51:08 +0100

    day 8, part 2

--- 2023/day8/README.md
+++ 2023/day8/README.md
@@ -6,3 +6,10 @@
 Time (mean ± σ):       7.8 ms ±   4.6 ms    [User: 2.5 ms, System: 2.8 ms]
 Range (min … max):     2.9 ms …  29.9 ms    185 runs
 ```
+
+## Benchmark Part 2
+
+```
+Time (mean ± σ):       8.3 ms ±   1.5 ms    [User: 4.7 ms, System: 1.7 ms]
+Range (min … max):     7.0 ms …  23.7 ms    184 runs
+```
--- 2023/day8/src/main.rs
+++ 2023/day8/src/main.rs
@@ -1,10 +1,32 @@
-use std::{fs, collections::HashMap};
+use std::{collections::HashMap, fs};
 
 fn parse_lines(i: &str) -> HashMap<&str, (&str, &str)> {
-    i.lines().map(|l| {
-        (l.get(0..3).unwrap(), (l.get(7..10).unwrap(), l.get(12..15).unwrap()))
-    }).collect()
+    i.lines()
+        .map(|l| {
+            (
+                l.get(0..3).unwrap(),
+                (l.get(7..10).unwrap(), l.get(12..15).unwrap()),
+            )
+        })
+        .collect()
 }
+
+pub fn lcm(nums: &[usize]) -> usize {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
+}
+
 fn part1(i: &str) -> i64 {
     let (instructions, last) = i.split_once("\n\n").unwrap();
     let s = parse_lines(last);
@@ -19,12 +41,12 @@ fn part1(i: &str) -> i64 {
                 'L' => {
                     current_loc = s.get(&current_loc).unwrap().0;
                     steps += 1;
-                },
+                }
                 'R' => {
                     current_loc = s.get(&current_loc).unwrap().1;
                     steps += 1;
-                },
-                _ => unreachable!("Letter not found")
+                }
+                _ => unreachable!("Letter not found"),
             };
             if current_loc == "ZZZ" {
                 break;
@@ -38,11 +60,57 @@ fn part1(i: &str) -> i64 {
     return steps;
 }
 
+fn part2(i: &str) -> usize {
+    let (instructions, last) = i.split_once("\n\n").unwrap();
+    let s = parse_lines(last);
+
+    let ops: Vec<_> = s.keys().filter(|k| k.ends_with("A")).collect();
+
+    let mut steps_comb = Vec::with_capacity(ops.len());
+    for o in ops.iter() {
+        let mut current_loc = **o;
+        let mut steps = 0;
+        loop {
+            for dir in instructions.chars() {
+                match dir {
+                    'L' => {
+                        // ops = ops
+                        //     .iter()
+                        //     .map(|o| &(s.get(*o).unwrap().0))
+                        //     .collect::<Vec<&&str>>();
+                        current_loc = s.get(&current_loc).unwrap().0;
+                        steps += 1;
+                    }
+                    'R' => {
+                        // ops = ops
+                        //     .iter()
+                        //     .map(|o| &(s.get(*o).unwrap().1))
+                        //     .collect::<Vec<&&str>>();
+                        current_loc = s.get(&current_loc).unwrap().1;
+                        steps += 1;
+                    }
+                    _ => unreachable!("Letter not found"),
+                };
+                // if ops.iter().all(|o| o.ends_with("Z")) {
+                //     break;
+                // }
+                if current_loc.ends_with("Z") {
+                    break;
+                }
+            }
+            if current_loc.ends_with("Z") {
+                break;
+            }
+        }
+        steps_comb.push(steps);
+    }
+    lcm(&steps_comb)
+}
+
 fn main() {
     let raw = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let out = part1(raw.trim());
+    let out = part2(raw.trim());
     println!("{out}");
-
 }
 
 #[cfg(test)]
